c09f70b32a21c3bf0741d9a78c987bc2d375e2f5
francoguidi01 <francoguidi1235@gmail.com>
2023-06-05 18:54:13 -0300

    commit de franco holanda

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.swing.*;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       
+    }
+}
